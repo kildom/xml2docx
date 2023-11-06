1a8bb5687c873acdaf37836bb904372b96b0bb69
kildom <kildom@users.noreply.github.com>
2023-11-06 11:33:56 +0100

    Tables documentation

--- src/xml2docx.ts
+++ src/xml2docx.ts
@@ -118,4 +118,13 @@ async function exec(templateFile: string, dataFile: string, docxFile: string, ba
 
     // TODO: In debug mode, generate JS file that creates document using docx.js API.
     //       Creating a new object: obj = new docx.SomeClass(...args); obj[constructInfoSymbol] = { className: 'SomeClass', args }
+    // TODO: Filters for multiline text:
+    //       1) trim, remove new lines and replace repeating whitespace
+    //       2) removes common indentation and trim
+    // TODO: Special type of element ":attr", that works similar to ":property", but adds string value to attributes.
+    //       Useful for multiline attributes and attributes with CDATA.
+    // TODO: reconsider renaming ":property" (maybe also ":attr")
+    // TODO: Simplify sections, footers, headers, styles by adding new tags for it.
+    //       header and footer can have attribute that tells if it is just for first page in section.
+    // TODO: Check if docx.js allows reusing the same image data
 }
